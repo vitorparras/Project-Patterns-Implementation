22f732d2c86c1e12f00874aef2f808b06f9a3bc6
Vitor Parras <vitorhugoparras@hotmail.com>
2024-08-11 22:38:48 -0300

    Added context to the project

---
+++ Architectural Patterns/MinimalApi/Infrastructure/MinimalContext.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class MinimalContext : DbContext
+    {
+        public MinimalContext(DbContextOptions<MinimalContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                  .UseInMemoryDatabase("Teste")
+                  .EnableSensitiveDataLogging();
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<TokenHistory> TokenHistories { get; set; }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
