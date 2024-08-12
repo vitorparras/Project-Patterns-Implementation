0974e854e7206869ee8eeb830903d1d37f610159
Vitor Parras <vitorhugoparras@hotmail.com>
2024-08-11 23:56:46 -0300

    Added dependency injection for services, and implemented endpoints for user management, login, and logout

---
+++ Architectural Patterns/MinimalApi/API/Endpoints/AuthEndpoints.cs
@@ -0,0 +1,17 @@
+﻿
+using Application.Services.Interfaces;
+
+namespace API.Endpoints
+{
+    public static class AuthEndpoints
+    {
+        public static void MapAuthEndpoints(this IEndpointRouteBuilder app)
+        {
+            var users = app.MapGroup("/api/auth");
+
+            users.MapPost("/Login", async (string email, string password,IAuthService authService) => await authService.LoginAsync(email, password));
+
+            users.MapPost("/Logout", async (string token, IAuthService authService) => await authService.LogoutAsync(token));
+        }
+    }
+}
---
+++ Architectural Patterns/MinimalApi/API/Endpoints/UserEndpoints.cs
@@ -0,0 +1,79 @@
+﻿
+using Application.Services.Interfaces;
+using Domain.DTO;
+
+namespace API.Endpoints
+{
+    public static class UserEndpoints
+    {
+        public static void MapUserEndpoints(this IEndpointRouteBuilder app)
+        {
+            var users = app.MapGroup("/api/users");
+
+            users.MapGet("/list", async (IUserService userService) =>
+            {
+                try
+                {
+                    var users = await userService.GetAllAsync();
+                    return Results.Ok(users);
+                }
+                catch (Exception ex)
+                {
+                    return Results.Problem(ex.Message);
+                }
+            });
+
+            users.MapGet("/{id:guid}", async (Guid id, IUserService userService) =>
+            {
+                try
+                {
+                    var user = await userService.GetByIdAsync(id);
+                    return user != null ? Results.Ok(user) : Results.NotFound();
+                }
+                catch (Exception ex)
+                {
+                    return Results.Problem(ex.Message);
+                }
+            });
+
+            users.MapPost("/add", async (UserAddDTO userDto, IUserService userService) =>
+            {
+                try
+                {
+                    await userService.AddAsync(userDto);
+                    return Results.Created();
+                }
+                catch (Exception ex)
+                {
+                    return Results.Problem(ex.Message);
+                }
+            });
+
+            users.MapPut("/update", async (UserDTO userDto, IUserService userService) =>
+            {
+                try
+                {
+                    await userService.UpdateAsync(userDto);
+                    return Results.Ok();
+                }
+                catch (Exception ex)
+                {
+                    return Results.Problem(ex.Message);
+                }
+            });
+
+            users.MapDelete("/{id:guid}", async (Guid id, IUserService userService) =>
+            {
+                try
+                {
+                    await userService.DeleteAsync(id);
+                    return Results.NoContent();
+                }
+                catch (Exception ex)
+                {
+                    return Results.Problem(ex.Message);
+                }
+            });
+        }
+    }
+}
