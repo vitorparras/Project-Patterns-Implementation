5f6975396bd9ce6faf861f3261f423891127bfa7
Vitor Parras <vitorhugoparras@hotmail.com>
2024-08-11 22:37:17 -0300

    Added Models

---
+++ Architectural Patterns/MinimalApi/Domain/Model/User.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Model.Bases;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Model
+{
+    public class User : BaseEntity
+    {
+        [Required]
+        public string Email { get; set; }
+        
+        [Required] 
+        public string Password { get; set; }
+
+        [Required, MaxLength(255)]
+        public string Name { get; set; }
+    }
+}
